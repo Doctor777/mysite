f2f6e371e38be19bf151ee162e1562b34e0126fb
Doctor777 <aptekasbt@gmail.com>
2017-12-11 21:25:34 +0200

    111217

--- components/Router.php
+++ components/Router.php
@@ -95,9 +95,9 @@ class Router
     function ErrorPage404()
     {
         $host = 'http://' . $_SERVER['HTTP_HOST'] . '/';
-        header('HTTP/1.1 404 Not Found');
-        header("Status: 404 Not Found");
-        header('Location:' . $host . '404');
+     //   header('HTTP/1.1 404 Not Found');
+      //  header("Status: 404 Not Found");
+        header('Location:' . $host . '404.html');
 
     }
 }
